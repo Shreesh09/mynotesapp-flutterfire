6ace2d491c02f74be78d32d93ded6597005c60af
Shreesh <shreeshnautiyal3@gmail.com>
2023-07-24 12:35:21 +0530

    fix: fixed firebase security rules

--- lib/services/cloud/firbase_cloud_storage.dart
+++ lib/services/cloud/firbase_cloud_storage.dart
@@ -26,25 +26,10 @@ class FirebaseCloudStorage {
     );
   }
 
-  Future<Iterable<CloudNote>> getNotes({required String ownerUserId}) async {
-    try {
-      return await notes
-          .where(
-            ownerUserIdFieldName,
-            isEqualTo: ownerUserId,
-          )
-          .get()
-          .then(
-              (value) => value.docs.map((doc) => CloudNote.fromSnapShot(doc)));
-    } catch (e) {
-      throw CouldNotGetAllNotesException();
-    }
-  }
-
-  Stream<Iterable<CloudNote>> allNotes({required String ownerUserId}) =>
-      notes.snapshots().map((event) => event.docs
-          .map((doc) => CloudNote.fromSnapShot(doc))
-          .where((note) => note.ownerUserId == ownerUserId));
+  Stream<Iterable<CloudNote>> allNotes({required String ownerUserId}) => notes
+      .where(ownerUserIdFieldName, isEqualTo: ownerUserId)
+      .snapshots()
+      .map((event) => event.docs.map((doc) => CloudNote.fromSnapShot(doc)));
 
   Future<void> updateNote({
     required String documentId,
